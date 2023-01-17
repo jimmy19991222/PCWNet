3929d0182022bae6dcb1569b1a6587da5340c2ea
Jieming Lou <347807497@qq.com>
2023-01-17 14:10:35 +0800

    add wo_fine version

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,9 @@
-from models.pwcnet import PWCNet_G, PWCNet_GC
+from models.pwcnet import PWCNet_G, PWCNet_GC, PWCNet_wo_fine_GC, PWCNet_wo_fine_G
 from models.loss import model_loss
 
 __models__ = {
     "gwcnet-g": PWCNet_G,
-    "gwcnet-gc": PWCNet_GC
+    "gwcnet-gc": PWCNet_GC,
+    "gwcnet-wo_fine_gc": PWCNet_wo_fine_GC,
+    "gwcnet-wo_fine_g": PWCNet_wo_fine_G
 }
